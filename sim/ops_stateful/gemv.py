d3d4b24f5cff4c4e92f8b7ec4021f77f2dd768fc
Louis Primeau <louis.primeau@mail.utoronto.ca>
2022-03-10 19:35:05 -0500

    remove conflicts

--- sim/ops_stateful/gemv.py
+++ sim/ops_stateful/gemv.py
@@ -6,7 +6,8 @@ class gemv(torch.autograd.Function):
     @staticmethod
     def forward(ctx, interface, alpha, A, x, beta, b):
         ctx.save_for_backward(alpha, A, x, beta, b)
-        return interface.vmm(torch.cat((x, torch.ones(1, 1))))
+        out = interface.vmm(torch.cat((x, torch.ones(1, 1))))
+        return out
         
     @staticmethod
     def backward(ctx, dx):
@@ -32,8 +33,6 @@ class GEMV(torch.nn.Module):
 
         self.A, self.b, self.alpha, self.beta = torch.nn.Parameter(A), torch.nn.Parameter(b), torch.nn.Parameter(alpha), torch.nn.Parameter(beta)
         
-
-
         self.rows, self.cols = self.A.size()
         assert b.size() == (self.rows, 1), "bias has incorrect shape {}, should be {}".format(bias.size(), (1, self.cols))
         assert alpha.size() == (1,), "alpha has incorrect shape {}, should be {}".format(alpha.size(), (1,))
@@ -41,11 +40,7 @@ class GEMV(torch.nn.Module):
 
         matrix = torch.cat((self.A.transpose(0, 1) * self.alpha, self.b.transpose(0, 1) * self.beta))
 
-        print(matrix)
         self.interface = sys.program(matrix) #crossbar interface
-        print(self.interface.weights)
-        for tix in self.interface.tickets:
-            print(tix.matrix)
         self.f = gemv() #autograd wrapper
 
     def forward(self, x):
